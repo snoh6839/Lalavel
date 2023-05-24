473937b5c2e3fdabbc86718eb4e8818a8ebfed9d
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-24 13:14:49 +0900

    php artisan db:seed

--- edu-laravel/database/seeders/CategorySeeder.php
+++ edu-laravel/database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -14,6 +15,17 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('categories')->insert(
+            ['no' => '0', 'name' => '카테고리1']
+            ,['no' => '1', 'name' => '카테고리2']
+            ,['no' => '2', 'name' => '카테고리3']
+            ,['no' => '3', 'name' => '카테고리4']
+            ,['no' => '4', 'name' => '카테고리5']
+            ,['no' => '5', 'name' => '카테고리6']
+            ,['no' => '6', 'name' => '카테고리7']
+            ,['no' => '7', 'name' => '카테고리8']
+            ,['no' => '8', 'name' => '카테고리9']
+            ,['no' => '9', 'name' => '카테고리10']
+        );
     }
 }
--- edu-laravel/database/seeders/DatabaseSeeder.php
+++ edu-laravel/database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(CategorySeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
